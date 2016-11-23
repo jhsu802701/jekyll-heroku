f1b9a29d5022846da7212cc30afe0aae792896d9
Jason Hsu <jhsu802701@jasonhsu.com>
2016-11-23 20:11:13 +0000

    Added server.sh

---
+++ server.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+cat /home/winner/shared/ports.txt
+echo '**********************'
+echo 'OPEN YOUR WEB BROWSER.'
+echo 'GO TO THE FOLLOWING URL:'
+echo 'http://<IP Address>:4000'
+echo 'If Docker is installed directly in your host OS, the IP address is localhost.'
+echo 'If you are using Docker Machine, use the IP address is probably 192.168.99.100.'
+echo '*************************************************************************'
+echo '---------------------------'
+echo 'jekyll serve --host 0.0.0.0'
+jekyll serve --host 0.0.0.0
